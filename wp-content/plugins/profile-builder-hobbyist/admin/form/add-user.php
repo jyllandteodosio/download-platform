a05f0bc0446f13b412b82bbe0fb85f7a826ce1d3
dianne delosreyes <https://diannedelosreyes@bitbucket.org>
2016-02-11 10:42:49 +0800

    fixed code layout and added edit user for operators plugin

--- wp-content/plugins/profile-builder-hobbyist/admin/form/add-user.php
+++ wp-content/plugins/profile-builder-hobbyist/admin/form/add-user.php
@@ -1,6 +1,4 @@
 <?php
 include_once( WPPB_PLUGIN_DIR.'/front-end/register.php' ); 
-echo wppb_front_end_register_handler( 
-    array (  )
-);
+echo wppb_front_end_register_handler(array (  ));
 ?>
--- wp-content/plugins/profile-builder-hobbyist/index.php
+++ wp-content/plugins/profile-builder-hobbyist/index.php
@@ -133,6 +133,9 @@ function wppb_hobbyist_plugin_init() {
         if (file_exists(WPPB_PLUGIN_DIR . '/admin/add-users.php')){
             include_once(WPPB_PLUGIN_DIR . '/admin/add-users.php');
         }
+        if (file_exists(WPPB_PLUGIN_DIR . '/admin/edit-users.php')){
+            include_once(WPPB_PLUGIN_DIR . '/admin/edit-users.php');
+        }
          
         // end of custom added field
 
