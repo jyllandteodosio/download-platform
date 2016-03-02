173430720f84ebc8a0136f38082c1a17cabddc22
dianne delosreyes <https://diannedelosreyes@bitbucket.org>
2016-03-02 10:47:29 +0800

    page cart

--- wp-content/themes/twentythirteen/page-cart.php
+++ wp-content/themes/twentythirteen/page-cart.php
@@ -9,8 +9,19 @@ get_header(); ?>
 
 echo "<pre>";
     print_r(get_custom_cart_contents('image'));
-echo "</pre><br>";
+echo "</pre><br>";?>
 
+<div class='col-md-4 col-sm-6 col-xs-6'>
+	<div class='panel panel-default'>
+		<div class='panel-heading ttip' title='{$fileTitle}'>asd</div>
+		<div class='panel-body text-center'>thumb</div>
+		<div class='panel-footer'>
+			<span class='btn btn-primary btn-sm btn-block btn-add-to-cart' data-file-id='' data-file-title='' data-file-path='' data-thumb='' data-post-id='' data-file-type='' data-user-id='' href='#'>Download</span>
+		</div>
+		<span class='btn btn-primary btn-sm btn-block btn-remove-to-cart' data-file-id='' data-user-id='' href='#'>Remove</span>
+	</div>
+</div>
+<?php
 echo "<pre>";
     print_r(get_custom_cart_contents('promo'));
 echo "</pre><br>";
