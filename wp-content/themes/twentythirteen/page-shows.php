2a49286cecac3badde2fcddeb5ebdc7b950e6553
dianne delosreyes <https://diannedelosreyes@bitbucket.org>
2016-03-03 12:05:18 +0800

    re-enabled gitignored files (htaccess, wpconfig, sql)

--- wp-content/themes/twentythirteen/page-shows.php
+++ wp-content/themes/twentythirteen/page-shows.php
@@ -5,8 +5,8 @@ get_header(); ?>
 <h2>Shows</h2>
 
 <?php 
-$assigned_show_category = 'entertainment,extreme';
-$letter_filter = 'L';
+$assigned_show_category = 'entertainment';
+$letter_filter = '';
 echo do_shortcode("[wpdm_category_custom id='{$assigned_show_category}' operator='IN' title='Shows' toolbar='1' order_by='title' order='asc' item_per_page='12' letter_filter = '{$letter_filter}' ]");?>
 
 <?php get_sidebar(); ?>
