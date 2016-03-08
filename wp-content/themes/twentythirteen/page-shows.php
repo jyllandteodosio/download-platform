3c340e2c3734f80de5501e61241aca819002a0b8
dianne delosreyes <https://diannedelosreyes@bitbucket.org>
2016-03-08 11:15:30 +0800

    updated TODOs

--- wp-content/themes/twentythirteen/page-shows.php
+++ wp-content/themes/twentythirteen/page-shows.php
@@ -5,7 +5,9 @@ get_header(); ?>
 <h2>Shows</h2>
 
 <?php 
+// TODO: fetch assigned show category from operator access table
 $assigned_show_category = 'entertainment';
+// TODO: wait frontend design for letter filter UI 
 $letter_filter = '';
 echo do_shortcode("[wpdm_category_custom id='{$assigned_show_category}' operator='IN' title='Shows' toolbar='1' order_by='title' order='asc' item_per_page='12' letter_filter = '{$letter_filter}' ]");?>
 
