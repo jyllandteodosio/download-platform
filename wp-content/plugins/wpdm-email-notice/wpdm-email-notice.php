a4adcd66011b5f9893c7a72120a3add90cc5ef48
dianne delosreyes <https://diannedelosreyes@bitbucket.org>
2016-04-25 15:50:33 +0800

    reformat email notice message

--- wp-content/plugins/wpdm-email-notice/wpdm-email-notice.php
+++ wp-content/plugins/wpdm-email-notice/wpdm-email-notice.php
@@ -42,7 +42,7 @@ function wpdm_check_new_files($id)
     $wpdm_promos_old = get_field( "add_promo_files" , $id) ? get_field( "add_promo_files" , $id) : array(); /* Old values from database */
     $wpdm_promos_new = $_POST['fields']['field_56bda9692303d'];	/* New values from form */
     array_pop($wpdm_promos_new); /* Remove extra array at the last index  */
- 
+ 	
     /* Restructure post data of promo files into key value pair */
     foreach ($wpdm_promos_new as $key => $value) {
         $wpdm_promos_new_id[$value['field_56bda9eb23040']] = $value['field_56bdaa2523043']; /* New values from form (key,value) */
@@ -52,7 +52,28 @@ function wpdm_check_new_files($id)
         $wpdm_promos_old_id[$value['id']] = $value['file_name']; /* Old values from database (key,value) */
     }
     $promos_diff=array_diff($wpdm_promos_new_id,$wpdm_promos_old_id); /* Check number New Promo files added */
-  
+  	
+  	// echo "<pre>";
+    // echo "<br><br>wpdm_files_old:<br>";
+ 	// print_r($wpdm_files_old);
+ 	// echo "<br><br>wpdm_files_new:<br>";
+ 	// print_r($wpdm_files_new);
+ 	// echo "<br><br>files_diff:<br>";
+ 	// print_r($files_diff);
+ 	// echo "<br><br>wpdm_promos_old:<br>";
+ 	// print_r($wpdm_promos_old);
+ 	// echo "<br><br>wpdm_promos_new:<br>";
+ 	// print_r($wpdm_promos_new);
+ 	// echo "<br><br>POST:<br>";
+ 	// print_r($_POST);
+ 	// echo "<br><br>wpdm_promos_old_id:<br>";
+ 	// print_r( $wpdm_promos_old_id);
+ 	// echo "<br><br>wpdm_promos_new_id:<br>";
+ 	// print_r( $wpdm_promos_new_id);
+ 	// echo "<br><br>promos_diff:<br>";
+ 	// print_r($promos_diff);
+    // echo "</pre>";
+
     if(count($files_diff) >= 1 || count($promos_diff) >= 1){
     	$categories = $_POST['tax_input']['wpdmcategory']; /* Get categories assigned */
 
@@ -85,18 +106,17 @@ function wpdm_check_new_files($id)
 		foreach ($users as $user) {
 			if(check_user_group_access($user, $operator_access)){
 				/* Check if there's a new promo file */
-				$promo_files = get_user_accessible_promos($user, $promos_diff, $wpdm_promos_new);
-				send_email_notice($user, $promo_files, $permalink);
-
-				print_r($promo_files);
+				$files['promo'] = get_user_accessible_promos($user, $promos_diff, $wpdm_promos_new);
+				$files['show'] = get_user_accessible_files($user, $files_diff, $wpdm_files_new);
+				send_email_notice($user, $files, $permalink);
 			}
 		}
-    	// die();
+    	// die("asd");
     }
 }
 
 
-function send_email_notice($user, $promo_files, $show_link){
+function send_email_notice($user, $files, $show_link){
 	$to = $user->user_email;
 	// $to = "diannekatherinedelosreyes@gmail.com";
 	$subject = 'RTL CBS Asia Notification - New files are available for you today!';
@@ -104,17 +124,27 @@ function send_email_notice($user, $promo_files, $show_link){
 
 	$message = '';
 	$message .= "Hi {$user->user_login},<br><br>";
-	$message .= "You have new files available for download today";
-	if(!empty($promo_files)){
-		$message .= ", including the following promo files:<br><br>";
-		foreach ($promo_files as $promo_file) {
-			$message .= " - ".$promo_file."<br>";
+	$message .= "<b>You have new files available for download today</b><br>";
+	if(!empty($files['promo'])){
+		$message .= "<br>Promo file/s:<br>";
+		$message .= "<ul>";
+		foreach ($files['promo'] as $file) {
+			$message .= "<li><a href='".$show_link."'>".$file."</a></li>";
 		}
+		$message .= "</ul>";
 	}
-	$message .= "<br><br>Check out the files here: <a href='".$show_link."'>Click here</a><br><br>
-				Thanks!<br><br>
+	if(!empty($files['show'])){
+		$message .= "Show file/s:<br>";
+		$message .= "<ul>";
+		foreach ($files['show'] as $file) {
+			$message .= "<li><a href='".$show_link."'>".$file."</a></li>";
+		}
+		$message .= "</ul>";
+	}
+	$message .= "<br><br><b>To view and download the files,</b> <a href='".$show_link."'>log on to the Operator Website</a><br><br>
+				Thanks!<br>
 				RTL CBS Asia Team";
-		
+	// echo $message;
 	// Start output buffering to grab smtp debugging output
 	ob_start();
 
@@ -125,7 +155,7 @@ function send_email_notice($user, $promo_files, $show_link){
 	$smtp_debug = ob_get_clean();
 		
 	// Output the response
-	echo "res-".$result;
+	// echo "res-".$result;
 }
 
 if (!function_exists('check_user_group_access')){
@@ -174,6 +204,31 @@ if (!function_exists('get_user_accessible_promos')){
 			}
 		}
 		return $promo_files;
+	}
+}
+
+if (!function_exists('get_user_accessible_files')){
+	/**
+	 * Get all oerator available newly uploaded promo files
+	 * @param  Object $user           	User
+	 * @param  Array  $new_promos_ids 	ID of newly uploaded promo file
+	 * @param  Array  $all_promos     	All current promos from the submitted form
+	 * @return Array                  	Array of promo file names
+	 */
+	function get_user_accessible_files($user, $new_files_ids, $all_files){
+		/* Check if there's a new promo uploaded */
+		$files = array();
+		if (count($new_files_ids) >= 1) {
+			foreach ($new_files_ids as $pd_key => $pd_value) {
+				foreach ($all_files as $wpn_key => $wpn_value) {
+					/* Check if KEYs are the same */
+					if($wpn_key == $pd_key){
+						array_push($files, $wpn_value);
+					}
+				}
+			}
+		}
+		return $files;
 	}
 }
 
