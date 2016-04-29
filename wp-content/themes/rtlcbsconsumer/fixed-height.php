414e40d4e176468f76179813c71358374daf881a
7M-AURA <aura.esteban@7thmedia.com>
2016-04-29 19:17:58 +0800

    Change scrollable template to Fixed Height Template; Finish Terms of Use

--- wp-content/themes/rtlcbsconsumer/fixed-height.php
+++ wp-content/themes/rtlcbsconsumer/fixed-height.php
@@ -1,17 +1,16 @@
 <?php
-/**
- * Template Name: Scrollable Content
- */
+// Template Name: Fixed Height
+
 get_header( 'rtl' ); ?>
 
-<div class="content-area content-scroll">
+<div class="content-area">
 	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 		<header class="entry-header">
 			<h1 class="entry-title"><?php the_title(); ?></h1> <?php edit_post_link(); ?>
 		</header>
-		<div class="entry-content">
-			<div class="simplebar">
+		<div class="fixed-height">
+			<div class="entry-content">
 				<?php the_content(); ?>
 				<div class="entry-links"><?php wp_link_pages(); ?></div>
 			</div>
--- wp-content/themes/rtlcbsconsumer/js/main.js
+++ wp-content/themes/rtlcbsconsumer/js/main.js
@@ -65,8 +65,9 @@
       }
    });
 
-   $( '.content-scroll .simplebar' ).simplebar({
-      autoHide: false
+   $( '.content-area .fixed-height' ).mCustomScrollbar({
+      axis: 'y',
+      'theme': 'red'
    });
 
    var videoPlayer;
@@ -97,57 +98,61 @@
    }
 
    $( window ).on( 'resize', function() {
-      if( $( window ).innerWidth() < 992 ) {
-         $( '.video-playlist' ).mCustomScrollbar( 'destroy' );
-         $( '.video-playlist-side' ).addClass( 'swiper-container' );
-         $( '.video-playlist-side .video-show-container' ).addClass( 'swiper-wrapper' );
-         $( '.video-playlist-side .video-show' ).addClass( 'swiper-slide' );
-         if (typeof videoPlayer == 'undefined' ) {
-            console.log('define videoplayer');
-            videoPlayer = new Swiper( '.video-playlist-side-container .video-playlist', {
-               slidesPerView: 4,
-               nextButton: '.video-playlist-container .swiper-button-next',
-               prevButton: '.video-playlist-container .swiper-button-prev',
-               breakpoints: {
-                  767: {
-                     slidesPerView: 3
-                  },
-                  479: {
-                     slidesPerView: 2
-                  },
-                  359: {
-                     slidesPerView: 1
+      if( $('.video-playlist').length >= 1 ) {
+         if( $( window ).innerWidth() < 992 ) {
+            $( '.video-playlist' ).mCustomScrollbar( 'destroy' );
+            $( '.video-playlist-side' ).addClass( 'swiper-container' );
+            $( '.video-playlist-side .video-show-container' ).addClass( 'swiper-wrapper' );
+            $( '.video-playlist-side .video-show' ).addClass( 'swiper-slide' );
+            if (typeof videoPlayer == 'undefined' ) {
+               console.log('define videoplayer');
+               videoPlayer = new Swiper( '.video-playlist-side-container .video-playlist', {
+                  slidesPerView: 4,
+                  nextButton: '.video-playlist-container .swiper-button-next',
+                  prevButton: '.video-playlist-container .swiper-button-prev',
+                  breakpoints: {
+                     767: {
+                        slidesPerView: 3
+                     },
+                     479: {
+                        slidesPerView: 2
+                     },
+                     359: {
+                        slidesPerView: 1
+                     }
                   }
-               }
+               });
+            }
+            videoPlayer.update();
+            videoPlayer.attachEvents();
+         } else {
+            if( videoPlayer ) videoPlayer.destroy( false, true );
+            $( '.video-playlist-side' ).removeClass( 'swiper-container' );
+            $( '.video-playlist-side .video-show-container' ).removeClass( 'swiper-wrapper' );
+            $( '.video-playlist-side .video-show' ).removeClass( 'swiper-slide' );
+            $( '.video-playlist-side-container .video-playlist' ).mCustomScrollbar({
+               theme: 'dark',
             });
-         }
-         videoPlayer.update();
-         videoPlayer.attachEvents();
-      } else {
-         if( videoPlayer ) videoPlayer.destroy( false, true );
-         $( '.video-playlist-side' ).removeClass( 'swiper-container' );
-         $( '.video-playlist-side .video-show-container' ).removeClass( 'swiper-wrapper' );
-         $( '.video-playlist-side .video-show' ).removeClass( 'swiper-slide' );
-         $( '.video-playlist-side-container .video-playlist' ).mCustomScrollbar({
-            theme: 'dark',
-         });
+      }
       }
    });
 
 
    /* Video Player
    ----------------------------------*/
-   var playerId = 'vimeoplayer'
-   var player = $f( document.getElementById( playerId ) );
-      player.addEvent('ready', function() {
-      player.api('play');
-   });
+   if( $( '.video-player' ).length >= 1 ) {
+      var playerId = 'vimeoplayer'
+      var player = $f( document.getElementById( playerId ) );
+         player.addEvent('ready', function() {
+         player.api('play');
+      });
 
-   $( '.video-show' ).on( 'click', function() {
-      $( '.video-show' ).removeClass( 'active' );
-      $( this ).addClass( 'active' );
-      var vimeoId = $(this).data('vimeo-id');
-      var src = '//player.vimeo.com/video/' + vimeoId + '?api=1&player_id=' + playerId;  
-      $( '#' + playerId ).prop( 'src', src);
-   });
+      $( '.video-show' ).on( 'click', function() {
+         $( '.video-show' ).removeClass( 'active' );
+         $( this ).addClass( 'active' );
+         var vimeoId = $(this).data('vimeo-id');
+         var src = '//player.vimeo.com/video/' + vimeoId + '?api=1&player_id=' + playerId;  
+         $( '#' + playerId ).prop( 'src', src);
+      });
+   }
 })( jQuery );
