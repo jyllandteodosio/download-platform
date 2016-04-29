6dc20b206f4c00bf3d8d2ad726fefaf767b1ad3e
7M-AURA <aura.esteban@7thmedia.com>
2016-04-29 16:07:03 +0800

    Responsive video player

--- wp-content/themes/rtlcbsconsumer/js/main.js
+++ wp-content/themes/rtlcbsconsumer/js/main.js
@@ -13,7 +13,7 @@
 		slidesPerView: 4,
 		spaceBetween: 6,
 		nextButton: '.swiper-button-next',
-            prevButton: '.swiper-button-prev',
+      prevButton: '.swiper-button-prev',
             breakpoints: {
             	320: {
             		slidesPerView: 1
@@ -34,17 +34,83 @@
       autoHide: false
    });
 
-   $( '.video-playlist' ).mCustomScrollbar({
-      theme: 'dark'
+   //* Loading this with screen width of 992px and above inside the condition doesn't work because of weird vimeo error. 
+   var videoPlayer = new Swiper( '.video-playlist', {
+      slidesPerView: 4,
+      nextButton: '.video-playlist-container .swiper-button-next',
+      prevButton: '.video-playlist-container .swiper-button-prev',
+      breakpoints: {
+         767: {
+            slidesPerView: 3
+         },
+         479: {
+            slidesPerView: 2
+         },
+         359: {
+            slidesPerView: 1
+         }
+      }
+   });
+   videoPlayer.destroy( false, true );
+
+   if( $( window ).innerWidth() >= 992 ) { 
+      $( '.video-playlist-side' ).removeClass( 'swiper-container' );
+      $( '.video-playlist-side .video-show-container' ).removeClass( 'swiper-wrapper' );
+      $( '.video-playlist-side .video-show' ).removeClass( 'swiper-slide' );
+      $( '.video-playlist' ).mCustomScrollbar({
+         theme: 'dark',
+      });
+   }
+
+   $( window ).on( 'resize', function() {
+      if( $( window ).innerWidth() < 992 ) {
+         $( '.video-playlist-side' ).addClass( 'swiper-container' );
+         $( '.video-playlist-side .video-show-container' ).addClass( 'swiper-wrapper' );
+         $( '.video-playlist-side .video-show' ).addClass( 'swiper-slide' );
+         $('.swiper-wrapper').removeAttr('style');
+         $('.swiper-slide').removeAttr('style');
+         if( typeof videoPlayer == 'undefined' ) {
+            videoPlayer = new Swiper( '.video-playlist', {
+               slidesPerView: 4,
+               nextButton: '.video-playlist-container .swiper-button-next',
+               prevButton: '.video-playlist-container .swiper-button-prev',
+               breakpoints: {
+                  767: {
+                     slidesPerView: 3
+                  },
+                  479: {
+                     slidesPerView: 2
+                  },
+                  359: {
+                     slidesPerView: 1
+                  }
+               }
+            });
+            
+         } else {
+            videoPlayer.attachEvents();
+         }
+         videoPlayer.update();
+         $( '.video-playlist' ).mCustomScrollbar( 'destroy' )
+      } else {
+         if( videoPlayer ) videoPlayer.destroy( false, true );
+         $( '.video-playlist-side' ).removeClass( 'swiper-container' );
+         $( '.video-playlist-side .video-show-container' ).removeClass( 'swiper-wrapper' );
+         $( '.video-playlist-side .video-show' ).removeClass( 'swiper-slide' );
+         $( '.video-playlist' ).mCustomScrollbar({
+            theme: 'dark',
+         });
+      }
    });
 
-   /* Video Player */
 
+   /* Video Player
+   ----------------------------------*/
    var playerId = 'vimeoplayer'
    var player = $f( document.getElementById( playerId ) );
       player.addEvent('ready', function() {
       player.api('play');
-   })
+   });
 
    $( '.video-show' ).on( 'click', function() {
       $( '.video-show' ).removeClass( 'active' );
