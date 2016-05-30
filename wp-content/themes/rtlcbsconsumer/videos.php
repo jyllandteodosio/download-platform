d82842c707b167c2df7e5cdf74637431c0de3d5f
dianne delosreyes <https://diannedelosreyes@bitbucket.org>
2016-05-30 11:26:24 +0800

    fixed videos page static initial featured video

--- wp-content/themes/rtlcbsconsumer/videos.php
+++ wp-content/themes/rtlcbsconsumer/videos.php
@@ -6,18 +6,31 @@ get_header( 'rtl' ); ?>
 <div class="content-area">
 	<div class="video-player-container video-player-side-container clearfix">
 		<div class="video-player">
-			<iframe id="vimeoplayer" width="700" height="400" src="http://player.vimeo.com/video/127580017?api=1&player_id=vimeoplayer" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
+		<?php
+			switch_to_blog( 1 );
+			$channel = 'entertainment';
+			$query_shows = getAllShows($channel);
+			if($query_shows->have_posts()):
+				$counter = 1;
+				while($query_shows->have_posts()) : $query_shows->the_post();
+				    $publish_date = get_post_meta(get_the_ID(), '__wpdm_publish_date', true);
+				    $expire_date = get_post_meta(get_the_ID(), '__wpdm_expire_date', true);
+				    if(checkPackageDownloadAvailabilityDate($publish_date, $expire_date)):?>
+				        <iframe id="vimeoplayer" width="700" height="400" src="http://player.vimeo.com/video/<?php the_field('vimeo_id'); ?>?api=1&player_id=vimeoplayer" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
+				    <?php 
+				    endif;
+				    break;
+				endwhile;
+			endif;?>
 		</div>
 		<div class="video-playlist-container video-playlist-side-container">
 			<div id="video-playlist" class="video-playlist video-playlist-side swiper-container">
 				<div class="video-show-container swiper-wrapper">
 					<?php
-						switch_to_blog( 1 );
-						$channel = 'entertainment';
-						$query_shows = getAllShows($channel);
-						if($query_shows->have_posts()):
+						$query_shows_2 = getAllShows($channel);
+						if($query_shows_2->have_posts()):
 							$counter = 1;
-				            while($query_shows->have_posts()) : $query_shows->the_post();
+				            while($query_shows_2->have_posts()) : $query_shows_2->the_post();
 				            	$publish_date = get_post_meta(get_the_ID(), '__wpdm_publish_date', true);
 				                $expire_date = get_post_meta(get_the_ID(), '__wpdm_expire_date', true);
 				                if(checkPackageDownloadAvailabilityDate($publish_date, $expire_date)):?>
@@ -30,10 +43,6 @@ get_header( 'rtl' ); ?>
 				        endif;
 				        restore_current_blog();
 					?>
-					<!-- <div class="video-show swiper-slide active" data-vimeo-id="127580017">
-						<span class="video-title">Billboard Music Awards 2015</span>
-						<img src="<?php echo get_template_directory_uri(); ?>/images/placeholders/billboard_500x280.png" class="video-thumbnail">
-					</div> -->
 				</div>
 			</div>
 			<div class="video-player-nav swiper-button-prev gradient-red hidden-md hidden-lg"></div>
