6f7576f7a47ee9f5cad080630ae7b1a756817a31
dianne delosreyes <https://diannedelosreyes@bitbucket.org>
2016-07-13 12:08:32 +0800

    fixed height of today's thumb

--- wp-content/themes/rtlcbsconsumer/front-page.php
+++ wp-content/themes/rtlcbsconsumer/front-page.php
@@ -83,9 +83,9 @@ get_header( 'rtl' ); ?>
 								$show_id = getPostIdByTitle($event->post_title);
 								if($show_id != ''):
 									$image = wp_get_attachment_image_src( get_post_thumbnail_id( $show_id), 'single-post-thumbnail' );?>
-									<p class="today-show-thumb-container">
+									<p class="today-show-thumb-container" style="background: url('<?php echo $image[0];?>') no-repeat top center; background-size:cover;">
 									<?php if($image[0] != ""): ?>
-										<img src="<?php echo $image[0];?>" alt="">
+										<!-- <img src="<?php echo $image[0];?>" alt=""> -->
 									<?php endif;?>
 									</p>
 								<?php endif;
