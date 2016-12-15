e9ded51067a5ff85f4e4cc3933688947be3db055
Dianne Katherine Delos Reyes <dianne.delosreyes@7thmedia.com>
2016-12-15 12:07:18 +0800

    fixed channel-setter code

--- wp-content/themes/rtlcbsconsumer/channel-setter.php
+++ wp-content/themes/rtlcbsconsumer/channel-setter.php
@@ -1,6 +1,9 @@
 <?php
   if(!isset($_GET['channel'])){
-    $_SESSION['channel'] = 'entertainment';
+    /* Set default toggled channel (used when the user is not yet clicking any channel.) */
+    if( !isset($_SESSION['channel']) ){
+      $_SESSION['channel'] = 'entertainment';
+    }
   }else if(isset($_GET['channel'])){
     $_SESSION['channel'] = $_GET['channel'];
   }
