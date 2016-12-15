b30d204d31f3b5a4909971a464bd5c4756727993
Dianne Katherine Delos Reyes <dianne.delosreyes@7thmedia.com>
2016-12-15 11:39:13 +0800

    revised channel-setter code, removed unnessesary functions

--- wp-content/themes/rtlcbsconsumer/channel-setter.php
+++ wp-content/themes/rtlcbsconsumer/channel-setter.php
@@ -1,17 +1,6 @@
 <?php
-switch_to_blog( 1 );
   if(!isset($_GET['channel'])){
-    /* Set default toggled channel (used when the user is not yet clicking any channel.) */
-    if(!isset($_SESSION['channel']) || $_SESSION['channel'] == 'none'){
-      if(set_channel_access('entertainment', true) == 0)
-        if( set_channel_access('extreme', true) == 0){
-          $_SESSION['channel'] = 'entertainment';
-          // $_SESSION['channel'] = 'none';
-        }
-    }
+    $_SESSION['channel'] = 'entertainment';
   }else if(isset($_GET['channel'])){
-    /* Set channel session based on toggled channel (used if the user clicks a channel in the header). */
-    if(set_channel_access($_GET['channel']) == 0)
-      $_SESSION['channel'] = 'none';
+    $_SESSION['channel'] = $_GET['channel'];
   }
-restore_current_blog();
