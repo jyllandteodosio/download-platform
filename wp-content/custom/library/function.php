cd5803d7be7aba9b72bbe56be7446374705ffa49
Tassha <tassha.nakagawa@7thmedia.com>
2017-02-06 16:07:43 +0800

    Sort promos according to upload date

--- wp-content/custom/library/function.php
+++ wp-content/custom/library/function.php
@@ -1417,7 +1417,7 @@ if (!function_exists('get_all_shows')) {
 
         $params = array(
             'post_type' => 'wpdmpro',
-             'orderby'     => 'modified',
+            'orderby'     => 'modified',
             'order'       => 'DESC',
             'status' => 'publish',
             'tax_query' => array(array(
@@ -1630,7 +1630,6 @@ if (!function_exists('getFeaturedShows')) {
                     'post_type' => 'wpdmpro',
                     'meta_key' => 'banner_order', 
                     'orderby' => 'meta_value',
-                    //'orderby'   => 'title',
                     'order'     => 'ASC',
                     'paged' => $paged,
                     'tax_query' => array(
@@ -1739,7 +1738,7 @@ if (!function_exists('getMonthsPromos')) {
         $args = array(
                     'post_type' => 'wpdmpro', 
                     'posts_per_page' => -1,
-                    'orderby' => 'post_date',
+                    'orderby' => get_post_meta(get_the_ID(), '__wpdm_publish_date', true),
                     'order' => 'DESC',
                     'tax_query' => array(
                         array(
