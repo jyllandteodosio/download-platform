61ff9ff475b02871fd048ba0ff2bb190ec1e1014
Tassha <tassha.nakagawa@7thmedia.com>
2017-03-06 11:01:29 +0800

    Fix for promo files missing upload date

--- wp-content/plugins/download-manager/libs/class.FileList.php
+++ wp-content/plugins/download-manager/libs/class.FileList.php
@@ -388,7 +388,8 @@ class FileList
                                     :  $sfileOriginal['file_name'];
 
 
-                $fileID = $prefix != self::$prefix_list['promos'] ? $fileID : $sfileOriginal['id'];
+                // $fileID = $prefix != self::$prefix_list['promos'] ? $fileID : $sfileOriginal['id'];
+                $fileID = $sfileOriginal['id'];
                 $thumb = "";
                 $ind = \WPDM_Crypt::Encrypt($sfile);
                 $operator_group_promo_access = isset($sfileOriginal['operator_group']) ? $sfileOriginal['operator_group'] : 'all';
